5b8f7b32383a0b010221ccaa2f7d0708abe96203
zzcc0912 <zzcc_tokyo@hotmail.com>
2019-07-16 22:40:46 +0900

    first commit

---
+++ src/main/java/com/anni/pj1/Pj1Application.java
@@ -0,0 +1,13 @@
+package com.anni.pj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj1Application.class, args);
+	}
+
+}
