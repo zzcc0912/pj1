faa593c64a5390bb2651248061890c9375a33966
zzcc0912 <zzcc_tokyo@hotmail.com>
2019-07-18 15:57:59 +0900

    2019/07/18 photoBranch first commit

--- src/test/java/com/anni/pj1/Pj1ApplicationTests.java
+++ src/test/java/com/anni/pj1/Pj1ApplicationTests.java
@@ -1,7 +1,9 @@
 package com.anni.pj1;
 
+import com.anni.pj1.domain.PhotoEntity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Pj1ApplicationTests {
 
+	@Autowired
+	private PhotoEntity pe;
+
 	@Test
 	public void contextLoads() {
+		pe.setAddress("/abc/esef");
+		System.out.println(pe.getAddress());
+
 	}
 
 }
